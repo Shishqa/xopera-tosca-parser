c92ad9bb1a06c5260eb4524f3cf15c6ed2162617
shishqa <shishqa.main@gmail.com>
2024-03-10 16:40:27 +0300

    wip

--- src/opera_tosca_parser/parser/tosca/v_1_3/definitions/policy_definition.py
+++ src/opera_tosca_parser/parser/tosca/v_1_3/definitions/policy_definition.py
@@ -319,6 +319,9 @@ class PolicyDefinition(CollectorMixin):
 
         return triggers
 
+    def concat(self, params):
+        return "".join([param.eval(self, '') for param in params])
+
     def get_property(self, params):
         host, prop, *rest = params
 
